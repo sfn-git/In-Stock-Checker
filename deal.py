f73a03f4afa2a8dd105288ef12ed6e71234d77de
Syed Nadeem <syedfnadeem323@gmail.com>
2020-11-16 13:15:21 -0500

    Check bestbuy and informs whether item is in stock or not. If not, then it will check until it is in stock. Window will open when it is in stock.

---
+++ deal.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+import os
+from random import randrange
+import time
+import datetime
+
+url = input("Enter Bestbuy URL: ")
+
+def callInStock():
+    seconds=randrange(10)
+    print("sleeping for {0} seconds".format(seconds))
+    time.sleep(seconds)
+    checkInStock()
+
+def checkInStock():
+    
+    options = Options()
+    options.headless = True
+    driver=webdriver.Firefox(options=options, executable_path="geckodriver.exe")
+    driver.get(url)
+    cart_button = driver.find_element_by_class_name("fulfillment-add-to-cart-button")
+    item_name = driver.find_element_by_class_name("sku-title")
+    status = cart_button.text
+    name = item_name.text
+
+    if status=="Sold Out":
+        currTime = datetime.datetime.now()
+        print("[{0}] Not in stock - {1}".format(currTime, name))
+        driver.close()
+        callInStock()
+    else:
+        print("In stock!")
+        command = "start firefox -new-window {0}".format(url)
+        os.system(command)
+        driver.close()
+
+checkInStock()
