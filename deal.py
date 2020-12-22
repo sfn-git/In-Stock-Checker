b1f57a1e4b746c9d9346f021382c4155266c6c41
Syed Nadeem <syedfnadeem323@gmail.com>
2020-12-21 23:03:31 -0500

    Reverted to old way of search due to memory leak

--- deal.py
+++ deal.py
@@ -1,12 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
-import selenium.webdriver.support.ui as ui
-from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import TimeoutException
+import os
 import datetime
 import sys
 import re
-import os
 import json
 
 userInput = True
@@ -50,29 +48,6 @@ else:
             urls[site].append(url)
         else:
             print("{0} is not a bestbuy or newegg url.".format(url))
-
-windows = []
-def closeWindows():
-    for window in windows:
-        currTime = datetime.datetime.now()
-        print("[{0}] Closing {1}".format(currTime, window.current_url))
-        window.close()
-        window.quit()
-
-for site in urls:
-    for url in urls[site]:
-        currTime = datetime.datetime.now()
-        print("[{1}] Opening {0}".format(url, currTime))
-        #initializes 
-        options = Options()
-        options.headless = True
-        driver=webdriver.Firefox(options=options, executable_path="geckodriver.exe")
-        driver.set_page_load_timeout(15)
-        driver.get(url)
-        windows.append(driver)
-        currTime = datetime.datetime.now()
-        print("[{0}] Opened".format(currTime))
-
 # Program that allows for wait
 def callInStock():
     global timeoutAttempts
@@ -90,59 +65,72 @@ def checkInStock():
     if length<=0:
         print("No URLS")
         return
+    
+    #initializes 
+    options = Options()
+    options.headless = True
+    driver=webdriver.Firefox(options=options, executable_path="geckodriver.exe")
+    driver.set_page_load_timeout(15)
     try:
-        for driver in windows:
-            #makes request to page
-            driver.refresh()
-            site = getWebsite(driver.current_url)
-            if site == "bestbuy":
-                cart_button = driver.find_element_by_class_name("fulfillment-add-to-cart-button") 
-                item_name = driver.find_element_by_class_name("sku-title") 
-                status = cart_button.text #Gets the text of the cart button
-                name = item_name.text #Gets item name
+        #loops through urls
+        for site in urls:
+            for url in urls[site]:
+                #makes request to page
+                driver.get(url)
+                if site == "bestbuy":
+                    cart_button = driver.find_element_by_class_name("fulfillment-add-to-cart-button") 
+                    item_name = driver.find_element_by_class_name("sku-title") 
+                    status = cart_button.text #Gets the text of the cart button
+                    name = item_name.text #Gets item name
 
-                #Checks if item is sold out
-                if status=="Coming Soon" or status=="Sold Out":
-                    currTime = datetime.datetime.now()
-                    print("[{0} ({3})] {2} - {1}".format(currTime, name, status, site))
-                else:
-                    currTime = datetime.datetime.now()
-                    print("[{1}] Needs Checking! - {0}".format(name, currTime))
-                    command = "start firefox -new-window {0}".format(driver.current_url)
-                    os.system(command)
-                    closeWindows()
-                    sys.exit()
-            elif site == "newegg":
-                cart_button = driver.find_element_by_id("ProductBuy") 
-                item_name = driver.find_element_by_class_name("product-title") 
-                status = cart_button.text #Gets the text of the cart button
-                name = item_name.text #Gets item name
-                #Checks if item is sold out
-                if status=="SOLD OUT" or status=="Auto Notify":
-                    currTime = datetime.datetime.now()
-                    print("[{0} ({3})] {2} - {1}".format(currTime, name, status, site))
-                else:
-                    currTime = datetime.datetime.now()
-                    print("[{1}] Needs Checking! - {0}".format(name, currTime))
-                    command = "start firefox -new-window {0}".format(driver.current_url)
-                    os.system(command)
-                    closeWindows()
-                    sys.exit()
+                    #Checks if item is sold out
+                    if status=="Coming Soon" or status=="Sold Out":
+                        currTime = datetime.datetime.now()
+                        print("[{0} ({3})] {2} - {1}".format(currTime, name, status, site))
+                    else:
+                        currTime = datetime.datetime.now()
+                        print("[{1}] Needs Checking! - {0}".format(name, currTime))
+                        command = "start firefox -new-window {0}".format(url)
+                        os.system(command)
+                        driver.close()
+                        driver.quit()
+                        sys.exit()
+                elif site == "newegg":
+                    cart_button = driver.find_element_by_id("ProductBuy") 
+                    item_name = driver.find_element_by_class_name("product-title") 
+                    status = cart_button.text #Gets the text of the cart button
+                    name = item_name.text #Gets item name
+                    #Checks if item is sold out
+                    if status=="SOLD OUT" or status=="Auto Notify":
+                        currTime = datetime.datetime.now()
+                        print("[{0} ({3})] {2} - {1}".format(currTime, name, status, site))
+                    else:
+                        currTime = datetime.datetime.now()
+                        print("[{1}] Needs Checking! - {0}".format(name, currTime))
+                        command = "start firefox -new-window {0}".format(url)
+                        os.system(command)
+                        driver.close()
+                        driver.quit()
+                        sys.exit()
+                elif site == "reddit":
+                    print("reddit")
+        #Closes the headless windows
+        driver.close()
+        driver.quit()
         #Calls wait function.
         callInStock()
     except TimeoutException as e:
         global timeoutAttempts
         timeoutAttempts+=1
         if timeoutAttempts == 3:
-            closeWindows()
             print("Timeout limit exceeded, ending program.")
             sys.exit()
         else:
             currTime = datetime.datetime.now()
             print("[{0}] Page Timed Out. Trying Again...".format(currTime))
+            driver.quit()
             checkInStock()
     except:
-        closeWindows()
         print("Exiting Program... (Either something went wrong or you ctrl+c)")
         sys.exit()
 
